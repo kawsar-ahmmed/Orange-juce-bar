c5e10208daca10f33c2b0c2ca5e1ece71007e4eb
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-01 00:48:38 +0600

    Font update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login/Login';
 import { createContext, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header';
 
 export const OnionContext = createContext();
 
 function App() {
   const [onion, setOnion] = useState(OnionContext);
-  console.log(onion)
+  // console.log(onion)
   return (
     <div>
       <OnionContext.Provider value={[onion, setOnion]}>
+        <Header></Header>
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/home' element={<Home></Home>}></Route>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Header.css'
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/logo2.png'
 
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar style={{ "height": "100px" }} collapseOnSelect expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={logo} height={35} alt="" />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#features">Login</Nav.Link>
-                            <Nav.Link href="#pricing">Sign Up</Nav.Link>
+                        <Nav className="justify-content-end menu-full">
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="/">Sign Up</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
